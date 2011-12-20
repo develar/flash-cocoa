9ebe582171ea360755832996ea6c912f12369230
develar <develop@gmail.com>
2011-12-20 20:29:50 +0400

    init adapt artFileTool to lion

--- artFileDecoder/src/Decoder.java
+++ artFileDecoder/src/Decoder.java
@@ -1,8 +1,12 @@
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.*;
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
@@ -11,32 +15,25 @@ import java.util.Map;
 import java.util.Set;
 
 public class Decoder {
-  private final String fp;
-  private RandomAccessFile artFile;
+  private final Set<String> duplicateGuard = new HashSet<String>();
+
+  private final ByteBuffer data;
   public int numOfImages = 0;
 
-  private short fileCount = 0;
-  private int tagCount = 0;
-  private short unknownShort = 0;
-  private int tagDescOffset = 0;
-  private int tagNamesOffset = 0;
-  private int fileDescOffset = 0;
-  private int fileDataSectionOffset = 0;
-  private int artFileStructOffset;
-  public short artRows;
+  private final short fileCount;
+  private final int tagCount;
+  private final int tagDescriptorsOffset;
+  private final int tagNamesOffset;
+  private final int fileDescriptorsOffset;
+  private final int fileDataSectionOffset;
+
   private File outputDir;
-  public short artColumns;
-  public int[] subimageOffsets;
-  public short[] subimageWidths;
-  public short[] subimageHeights;
-  public String[] names = new String[183];
+  public final CharSequence[] names;
   public int[] tags = new int[8];
-  private int numOfTags = 0;
-  public int[] imageArray;
+  private final int numOfTags = 0;
 
   @SuppressWarnings("ResultOfMethodCallIgnored")
-  public Decoder(String filepath, String output) {
-    fp = filepath;
+  public Decoder(String filepath, String output) throws IOException {
     outputDir = new File(output);
     if (outputDir.exists()) {
       outputDir.renameTo(new File(output + "_old_" + Math.random() * 100));
@@ -44,206 +41,123 @@ public class Decoder {
 
     outputDir = new File(output);
     outputDir.mkdirs();
-  }
 
-  public Decoder(String filepath) {
-    fp = filepath;
-  }
-
-  public void openFile()
-    throws IOException {
-    artFile = new RandomAccessFile(fp, "rw");
-  }
-
-  public void getAttributes() throws IOException {
-    fileCount = Short.reverseBytes(artFile.readShort());
-    unknownShort = artFile.readShort();
-    tagCount = Integer.reverseBytes(artFile.readInt());
-    tagDescOffset = Integer.reverseBytes(artFile.readInt());
-    tagNamesOffset = Integer.reverseBytes(artFile.readInt());
-    fileDescOffset = Integer.reverseBytes(artFile.readInt());
-    fileDataSectionOffset = Integer.reverseBytes(artFile.readInt());
-  }
-
-  public int getNumOfFiles() {
-    return fileCount;
-  }
-
-  public short getUnknownFieldInHeader() {
-    return unknownShort;
-  }
-
-  public int getTagCount() {
-    return tagCount;
-  }
-
-  public int getNumOfTags() {
-    return numOfTags;
-  }
-
-  public int getArtOffsetAndSetTags(int fileNum) throws IOException {
-    artFile.seek(fileDescOffset + 12 * fileNum);
-    artFileStructOffset = (fileDataSectionOffset + Integer.reverseBytes(artFile.readInt()));
-
-    for (int i = 0; i < 8; i++) {
-      int curTag = artFile.readByte() & 0xFF;
-      if (curTag == 0) {
-        numOfTags = (i - 1);
-        break;
-      }
-      tags[i] = curTag;
-    }
-
-    return artFileStructOffset;
-  }
-
-  public void MakeNameArray()
-    throws IOException {
-    for (int l = 0; l < 150; l++) {
-      int numOfChars = 0;
-      artFile.seek(tagDescOffset + 8 * l);
-      int offset = Integer.reverseBytes(artFile.readInt());
-      int index = artFile.readByte() & 0xFF;
-
-      artFile.seek(offset + tagNamesOffset);
-
-      while (artFile.readByte() != 0) {
-        numOfChars++;
-      }
-      artFile.seek(artFile.getFilePointer() - (numOfChars + 1));
-      byte[] chars = new byte[numOfChars];
-      artFile.read(chars);
-      String name = new String(chars, "UTF8");
-      names[index] = name;
-    }
-  }
-
-  public void readArtData()
-    throws IOException {
-    artFile.seek(artFileStructOffset);
-    artRows = Short.reverseBytes(artFile.readShort());
-
-    artColumns = Short.reverseBytes(artFile.readShort());
-
-    numOfImages = (artRows * artColumns);
-
-    artFile.readInt();
-    artFile.readInt();
-    artFile.readInt();
-    artFile.readInt();
-    artFile.readInt();
-    artFile.readInt();
-    artFile.readInt();
-    subimageOffsets = new int[9];
-    int i = 0;
-
-    while (i < 9) {
-      subimageOffsets[i] = Integer.reverseBytes(artFile.readInt());
-
-      i++;
-    }
-    subimageWidths = new short[9];
-    i = 0;
-
-    while (i < 9) {
-      subimageWidths[i] = Short.reverseBytes(artFile.readShort());
-
-      i++;
-    }
-    i = 0;
-
-    subimageHeights = new short[9];
-    while (i < 9) {
-      subimageHeights[i] = Short.reverseBytes(artFile.readShort());
-
-      i++;
-    }
-  }
-
-  private final Set<String> duplicateGuard = new HashSet<String>();
-
-  @SuppressWarnings({"ResultOfMethodCallIgnored"})
-  public void writeImages(int fileNum) throws IOException {
-    MessageDigest md;
+    final File file = new File(filepath);
+    data = ByteBuffer.allocate((int)file.length());
+    data.order(ByteOrder.LITTLE_ENDIAN);
+    final FileInputStream inputStream = new FileInputStream(file);
+    final FileChannel channel = inputStream.getChannel();
     try {
-      md = MessageDigest.getInstance("SHA-512");
+      while (data.hasRemaining()) {
+        channel.read(data);
+      }
+      data.clear();
     }
-    catch (NoSuchAlgorithmException e) {
-      e.printStackTrace();
-      return;
-    }
-
-    int duplicateCount = 0;
-    for (int j = 0; j < numOfImages; j++) {
-      artFile.seek(getArtOffsetAndSetTags(fileNum) + subimageOffsets[j]);
-      if ((subimageWidths[j] == 0) || (subimageHeights[j] == 0)) {
-        continue;
-      }
-      imageArray = new int[subimageWidths[j] * subimageHeights[j]];
-      byte[] hashBuffer = new byte[imageArray.length * 4];
-      int k = 0;
-      int h = 0;
-      while (k < imageArray.length) {
-        final int v = artFile.readInt();
-        imageArray[k] = v;
-
-        hashBuffer[h++] = (byte)((v >>> 24) & 0xFF);
-        hashBuffer[h++] = (byte)((v >>> 16) & 0xFF);
-        hashBuffer[h++] = (byte)((v >>> 8) & 0xFF);
-        hashBuffer[h++] = (byte)(v & 0xFF);
-
-        k++;
-      }
-
-      final String digest = convertToHex(md.digest(hashBuffer));
-      md.reset();
-      if (duplicateGuard.contains(digest)) {
-        duplicateCount++;
-        continue;
-      }
-
-      duplicateGuard.add(digest);
-      
-      BufferedImage image = new BufferedImage(subimageWidths[j], subimageHeights[j], 2);
-      image.setRGB(0, 0, subimageWidths[j], subimageHeights[j], imageArray, 0, subimageWidths[j]);
-
-      File dir = new File(outputDir, names[tags[0]] + "/" + names[tags[1]]);
-      dir.mkdirs();
-      StringBuilder filenameBuilder = new StringBuilder();
-      for (int i = 2; i <= numOfTags; i++) {
-        filenameBuilder.append(names[tags[i]]).append(".");
-      }
-      final String subLocalPath = filenameBuilder.toString();
-      final String key = dir.getPath() + "/" + subLocalPath;
-      filenameBuilder.append("png");
-      final String localPath = filenameBuilder.toString();
-
-      Integer counter = pathCounter.get(key);
-      File file;
-      if (counter == null) {
-        file = new File(dir, localPath);
-        if (file.exists()) {
-          counter = 0;
-
-          file.renameTo(new File(dir, subLocalPath + counter++ + ".png"));
-          file = new File(dir, subLocalPath + counter++ + ".png");
-          pathCounter.put(key, counter);
-        }
-      }
-      else {
-        file = new File(dir, subLocalPath + counter++ + ".png");
-        if (file.exists()) {
-          throw new IOException();
-        }
-
-        pathCounter.put(key, counter);
-      }
-
-      ImageIO.write(image, "png", file);
+    finally {
+      inputStream.close();
     }
 
-    //System.out.print("duplicated: " + duplicateCount + "\n");
+    fileCount = data.getShort();
+    // skip maxDepth
+    data.position(data.position() + 2);
+    tagCount = data.getInt();
+    tagDescriptorsOffset = data.getInt();
+    tagNamesOffset = data.getInt();
+    fileDescriptorsOffset = data.getInt();
+    fileDataSectionOffset = data.getInt();
+
+    // 1-based
+    names = new CharSequence[tagCount + 1];
   }
+
+  public void decode() throws IOException {
+    readTagDescriptors();
+
+    final ComponentColorModel colorModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_sRGB), true, false, Transparency.TRANSLUCENT, DataBuffer.TYPE_BYTE);
+    final int[] bandOffsets = {2, 1, 0, 3};
+
+    for (int i = 0; i < fileCount; i++) {
+      data.position(fileDescriptorsOffset + ((4 + 8) * i));
+      final int fileDataOffset = data.getInt();
+      final int[] tags = new int[8];
+      for (int j = 0; j < 8; j++) {
+        tags[j] = data.get() & 0xff;
+      }
+
+      data.position(fileDataSectionOffset + fileDataOffset);
+      final int artRows = data.getShort();
+      final int artColumns = data.getShort();
+      // skip unknown
+      data.position(data.position() + 28);
+      
+      final int[] subimageOffsets = readNumericArray(9, false);
+      final int[] subimageWidths = readNumericArray(9, true);
+      final int[] subimageHeights = readNumericArray(9, true);
+
+      StringBuilder filename = new StringBuilder();
+      for (int y : tags) {
+        if (y == 0) {
+          continue;
+        }
+
+        filename.append('/').append(names[y]);
+      }
+
+      final byte[] bytes = data.array();
+      final int imageCount = artRows * artColumns;
+      for (int subImageIndex = 0; subImageIndex < imageCount; subImageIndex++) {
+        final int w = subimageWidths[subImageIndex];
+        final int h = subimageHeights[subImageIndex];
+        if (w <= 0 || h <= 0) {
+          continue;
+        }
+
+        final byte[] bgra = new byte[w * h * 4];
+        System.arraycopy(bytes, fileDataSectionOffset + fileDataOffset + subimageOffsets[subImageIndex], bgra, 0, bgra.length);
+        BufferedImage image = new BufferedImage(colorModel, (WritableRaster)Raster.createRaster(new PixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, w, h, 4, w * 4, bandOffsets), new DataBufferByte(bgra, w * h), null), false, null);
+        File file = new File(outputDir, filename.substring(1) + '_' + subImageIndex + ".png");
+        //noinspection ResultOfMethodCallIgnored
+        file.getParentFile().mkdirs();
+        ImageIO.write(image, "png", file);
+      }
+    }
+  }
+
+  private int[] readNumericArray(final int length, final boolean isShort) {
+    final int[] r = new int[length];
+    for (int i = 0; i < length; i++) {
+      r[i] = isShort ? data.getShort() : data.getInt();
+    }
+    return r;
+  }
+
+  private void readTagDescriptors() throws IOException {
+    int tagDescriptorOffset = tagDescriptorsOffset;
+    final byte[] bytes = data.array();
+    final Charset utf8 = Charset.forName("utf-8");
+    for (int i = 0; i < tagCount; i++) {
+      final int dNameOffset = data.getInt(tagDescriptorOffset);
+      tagDescriptorOffset += 4;
+      final int tagIndex = data.get(tagDescriptorOffset) & 0xff;
+      tagDescriptorOffset += 4;
+
+      final int nameOffset = tagNamesOffset + dNameOffset;
+      int stringLength = 0;
+      while (bytes[nameOffset + stringLength] != 0) {
+        stringLength++;
+      }
+
+      data.mark();
+      data.position(nameOffset);
+      data.limit(nameOffset + stringLength);
+
+      names[tagIndex] = utf8.decode(data);
+      
+      data.reset();
+      data.limit(data.capacity());
+    }
+  }
+
 
   private final Map<String,Integer> pathCounter = new HashMap<String, Integer>();
 
--- artFileDecoder/src/Main.java
+++ artFileDecoder/src/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 public class Main {
   public static void main(String[] args)
     throws IOException {
-    boolean encode = false;
     //if (args.length < 3) {
     //  System.out.println("Usage: java -jar ArtFileTool.jar -encode artFiles oldArtFile outputArtFile");
     //  System.out.println("Usage: java -jar ArtFileTool.jar -decode ArtFile folderToOutput");
@@ -18,29 +17,13 @@ public class Main {
     //  System.exit(1);
     //}
 
-    if (encode) {
-      Encoder artEncoder = new Encoder(args[1], args[2], args[3]);
-      artEncoder.setUpEnv();
-      artEncoder.createHeader();
-      artEncoder.copyOldStuff();
-      for (int i = 0; i < artEncoder.getNumOfFiles(); i++) {
-        artEncoder.writeNewImages(i);
-      }
-      System.out.println("ArtFile.bin successfully created in " + args[3]);
-    }
-    else {
-      //Decoder artDecoder = new Decoder(args[1], args[2]);
-      Decoder artDecoder = new Decoder("/Users/develar/ArtFile.bin", "../artFiles");
-      artDecoder.openFile();
-      artDecoder.getAttributes();
 
-      artDecoder.MakeNameArray();
-      for (int i = 0; i < artDecoder.getNumOfFiles(); i++) {
-        artDecoder.getArtOffsetAndSetTags(i);
-        artDecoder.readArtData();
-        artDecoder.writeImages(i);
-      }
-      //System.out.println("ArtFile.bin Successfully Decoded to " + args[2]);
-    }
+    //Decoder artDecoder = new Decoder(args[1], args[2]);
+    Decoder decoder = new Decoder("/Users/develar/ArtFile.bin", "/Users/develar/test/artFiles");
+    decoder.decode();
+
+
+    //System.out.println("ArtFile.bin Successfully Decoded to " + args[2]);
+
   }
 }
